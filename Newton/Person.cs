a75320a3e4dd062d33720f598764e82e69ef6e03
KortSkæg <KortSkg@KORTSKG-PC>
2016-11-22 13:09:20 +0100

    Add project files.

---
+++ Newton/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newton
+{
+    class Person
+    {
+
+        public string Navn { get; set; }
+        public int Tlf { get; set; }
+        public string Email { get; set; }
+
+
+
+    }
+}
---
+++ Newton/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person() { Navn = "Mathias", Tlf = 12345678, Email = "Mig@mig.mig" };
+            string jsonText = JsonConvert.SerializeObject(person1);
+            Console.WriteLine(jsonText);
+            Console.ReadLine();
+        }
+    }
+}
